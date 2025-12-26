53d076acbddbd123f1477da6eb51f8eb2a2fef8e
Антон Фёдоров <afedorov@peremena.ru>
2025-12-26 12:41:43 +0300

    Баг с асинх(опять)

--- main.py
+++ main.py
@@ -1,43 +1,33 @@
+from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, filters, CallbackQueryHandler
+from bot.comands import ReservationBot
+from dotenv import load_dotenv
+from redis_config import redis_helpers
 import os
 import asyncio
-from dotenv import load_dotenv
-from telegram.ext import (
-    ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
-)
-from bot.comands import ReservationBot
-from redis_config import redis_helpers
 
 load_dotenv()
+
 TOKEN = os.getenv("TOKEN")
 
 
-async def start_listener(bot: ReservationBot):
-    await redis_helpers.listen_new_reservations(bot.new_reservation_notification)
-
-
 def main():
-    # Создаём приложение
     app = ApplicationBuilder().token(TOKEN).build()
-
-    # Инициализируем бота
     bot = ReservationBot(app)
-
-    # Регистрируем обработчики
+    
     app.add_handler(CommandHandler("start", bot.start))
     app.add_handler(CallbackQueryHandler(bot.callback))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.CONTACT, bot.text_handler))
     app.add_handler(MessageHandler(filters.CONTACT, bot.number_handler))
     app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, bot.web_app_handler))
 
-    async def on_startup(app):
-        print("Бот успешно инициализирован и запущен")
-        asyncio.create_task(start_listener(bot))
 
-    app.post_init = on_startup
+    async def start_listener():
+        await redis_helpers.listen_new_reservations(bot.new_reservation_notification)
 
-    # Запуск бота
+    app.job_queue.run_once(lambda ctx: asyncio.create_task(start_listener()), when=0)
+
+    print("Бот запущен")
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
