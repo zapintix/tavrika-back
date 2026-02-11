a8e61ebfcac6556cfe12952572895d2ac0960592
Антон <fedorovanton33@gmail.com>
2026-02-11 11:19:29 +0300

    некий прикол x2

--- api_main.py
+++ api_main.py
@@ -9,7 +9,10 @@ app = FastAPI(title="Reservation API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://reserve.localcafe.ru"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
